7e5b5797a5abc2b9baa58f6472bf3554723ba58f
Ryo <58804120+udemy1013@users.noreply.github.com>
2024-07-18 20:17:05 +0900

    monthlysettings pageの修正

--- src/app/dashboard/reservations/monthly-settings/[year]/[month]/page.tsx
+++ src/app/dashboard/reservations/monthly-settings/[year]/[month]/page.tsx
@@ -1,5 +1,11 @@
 import MonthlyReceptionSettingsDetail from "@/sections/Dashboard/reservation/monthly-settings/month/monthly-setting-view";
 
-export default function Login() {
-  return <MonthlyReceptionSettingsDetail />;
+export default function monthlysettings() {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear().toString();
+  const currentMonth = (currentDate.getMonth() + 1).toString().padStart(2, "0");
+
+  return (
+    <MonthlyReceptionSettingsDetail year={currentYear} month={currentMonth} />
+  );
 }
