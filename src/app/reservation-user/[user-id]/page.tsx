d0adacbacb06f7686e05032f073b74d6c83634a7
Ryo <58804120+udemy1013@users.noreply.github.com>
2024-08-01 20:49:51 +0900

    [user-id]/page.tsxの修正

--- src/app/reservation-user/[user-id]/page.tsx
+++ src/app/reservation-user/[user-id]/page.tsx
@@ -1,11 +1,7 @@
 "use client";
 
 import ReservationRoot from "@/sections/reservation-user/ReservationRoot";
-import { useParams } from "next/navigation";
 
 export default function ReservationUserPage() {
-  const params = useParams();
-  const userId = params.id as string;
-
-  return <ReservationRoot userId={userId} />;
+  return <ReservationRoot />;
 }
