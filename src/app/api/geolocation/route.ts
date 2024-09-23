d7290810c468f75624fcb31898ff8ed9855da10c
Ryo <58804120+udemy1013@users.noreply.github.com>
2024-09-23 18:35:09 +0900

    use clientエラーの修正

--- src/app/api/geolocation/route.ts
+++ src/app/api/geolocation/route.ts
@@ -1,9 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { geolocation } from "@vercel/edge";
 
-export const config = {
-  runtime: "edge",
-};
+export const runtime = "edge";
 
 export async function GET(request: NextRequest) {
   const { city, country, region } = geolocation(request);
