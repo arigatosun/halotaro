07ea549a0c2893a8df804d5bf78214f047829cb2
tsuchiga <t.koushi@arigatosun.com>
2024-12-27 16:35:00 +0900

    builderror修正

--- src/types/menuItem.ts
+++ src/types/menuItem.ts
@@ -9,6 +9,8 @@ export type MenuItem = {
   is_reservable?: boolean;
   created_at?: string; // or Date
   category_id?: number | null;
+  category: string;
+  isCoupon?: boolean;
   // JOIN した結果「categories」というオブジェクトが入るかもしれない
   categories?: {
     id: number;
